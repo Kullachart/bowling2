354ed473050b07381572d63da3391234428e08b4
Bowling <kullachart@ars.co.th>
2016-10-13 13:36:07 +0700

    ShowBig OK

--- app/src/main/java/appewtc/masterung/bowlingproduce/DetailActivity.java
+++ app/src/main/java/appewtc/masterung/bowlingproduce/DetailActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.bowlingproduce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -53,6 +54,20 @@ public class DetailActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        // Image Controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(DetailActivity.this, ShowBigActivity.class);
+                intent.putExtra("Icon", getIntent().getStringExtra("Icon"));
+                startActivity(intent);
+
+
+            }
+        });
+
 
 
 
