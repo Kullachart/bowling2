fa15b2cc93380f71292418c59391932c642f95b4
Bowling <kullachart@ars.co.th>
2016-10-13 11:56:34 +0700

    Listview

--- app/src/main/java/appewtc/masterung/bowlingproduce/Main2Activity.java
+++ app/src/main/java/appewtc/masterung/bowlingproduce/Main2Activity.java
@@ -11,18 +11,21 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class Main2Activity extends AppCompatActivity {
 
     // Explicit
     private String urlJson = "http://203.147.24.71/ars/get_cat.php";
-
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        ListView listView = (ListView) findViewById(R.id.livShowCat);
+        listView = (ListView) findViewById(R.id.livShowCat);
 
         // Create Listview
         SynCat synCat = new SynCat(Main2Activity.this);
@@ -67,6 +70,30 @@ public class Main2Activity extends AppCompatActivity {
             super.onPostExecute(s);
 
             Log.d("13octV1", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                String[] catStrings = new String[jsonArray.length()];
+                String[] imageStrings = new String[jsonArray.length()];
+                String[] urlStrings = new String[jsonArray.length()];
+
+                for (int i=0;i<jsonArray.length();i++) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    catStrings[i] = jsonObject.getString("Category");
+                    imageStrings[i] = jsonObject.getString("Image");
+                    urlStrings[i] = jsonObject.getString("urlJSON");
+
+                }  // For
+
+                CatAdapter catAdapter = new CatAdapter(context, catStrings, imageStrings);
+                listView.setAdapter(catAdapter);
+
+            }catch (Exception e){
+                Log.d("13octV1", "e onPost ==>" + e.toString());
+            }
 
 
 
